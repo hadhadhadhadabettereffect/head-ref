383f284688f5e1ff6c9a581a290a68fead56355f
eric birkeland <ericjbirkeland@gmail.com>
2018-04-05 16:09:03 -0400

    importing multiple files from threejs dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
+const path = require("path");
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const flatten = require("gulp-flatten");
 const source = require("vinyl-source-stream");
 const browserify = require("browserify");
 const tsify = require("tsify");
 const package = require("./package.json");
+
+const threeDir = "./node_modules/three/";
+const threeFiles = [
+    "node_modules/three/build/three.js",
+    "node_modules/three/examples/js/controls/OrbitControls.js"
+];
+// relative paths to three.js files from dist/index.html
+var jsImports = threeFiles.map((filePath) => {
+    return "js/" + path.basename(filePath);
+});
 
 gulp.task("js", function () {
     return browserify({
@@ -24,14 +36,17 @@ gulp.task("html", function () {
         .pipe(pug({
             data: {
                 title: package.name,
-                threepath: "js/three.js"
+                mainjs: "js/main.js",
+                jsImports: jsImports
             }
         }))
         .pipe(gulp.dest("dist"));
 });
 
+// copy three.js files to dist dir
 gulp.task("static", function () {
-    return gulp.src("node_modules/three/build/three.js")
+    return gulp.src(threeFiles, { base: threeDir })
+        .pipe(flatten())
         .pipe(gulp.dest("dist/js"));
 });
 
